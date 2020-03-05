3827e385eeb2faa1cb0c1fd75351c6f8f1a4759b
Lise Braaten <lisebraaten@gmail.com>
2020-03-05 11:09:21 -0800

    added exception errors

--- samplingsimulatorpy/draw_samples.py
+++ samplingsimulatorpy/draw_samples.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def draw_samples(pop, reps, n_s):
 
     """
@@ -18,7 +21,25 @@ def draw_samples(pop, reps, n_s):
     --------
     >>> pop = generate_virtual_pop(100, "Variable", normal, 0, 1)
     >>> samples = draw_samples(pop, 3, [5, 10, 15, 20])
-    """
+    """    
+    
+    #Check population input is a date frame with at least one value
+    if (len(pop) <=0 or not isinstance(pop, pd.DataFrame)):
+        raise TypeError("Population input is not a valid data frame")
+    
+    #Check number of reps is an integer
+    if not isinstance(reps, int):
+        raise TypeError("Number of replications input must be an integer value")
+        
+    #Check number of reps is positive
+    if reps <= 0:
+        raise ValueError("Number of replications must be greater than 0")
+    
+    #Check all values in sample size array are integers
+    for i in n_s:
+        if not isinstance(i, int):
+            raise TypeError("At least one value in sample size array is not an integer value")
+    
     samples = []
     rep_list = []
 
