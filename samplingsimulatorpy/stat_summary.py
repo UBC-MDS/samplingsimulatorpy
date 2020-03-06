bfb90173ad170a565d5884fdafea27c6a0d04185
tguo9 <tguo9@dons.usfca.edu>
2020-03-06 13:01:07 -0800

    add basic functions to stat_summary

--- samplingsimulatorpy/stat_summary.py
+++ samplingsimulatorpy/stat_summary.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 def stat_summary(population, samples, parameter):
     """
     This function creates a summary stats for population, samples and parameter(s) of interest
@@ -20,4 +20,11 @@ def stat_summary(population, samples, parameter):
     >>> from samplingsimulatorpy import stat_summary
     >>> stat_summary(pop, samples, parameter)
     """
-    print(np.mean(population))
+
+    new_dict = {'data' : ['pop', 'samples']}
+
+    for i in range(0, len(parameter)):
+
+        new_dict[parameter[i]] = [eval(parameter[i])(population)[0], eval(parameter[i])(samples)[0]]
+
+    return(pd.DataFrame(new_dict).set_index('data'))
