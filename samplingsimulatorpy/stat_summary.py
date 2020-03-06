5be28fe37f8156bf3e530f53e73303514ee7d2fb
tguo9 <tguo9@dons.usfca.edu>
2020-03-05 23:10:59 -0800

    add stat_summary.py placeholder file

---
+++ samplingsimulatorpy/stat_summary.py
@@ -0,0 +1,22 @@
+def stat_summary(population, samples, parameter):
+    """
+    This function creates a summary stats for population, samples and parameter(s) of interest
+    Parameters
+    ----------
+    population : pd.DataFrame
+        The virtual population
+    samples : pd.DataFrame
+        The drawed samples
+    parameter : list
+        The list of parameters
+    Returns
+    -------
+    pd.DataFrame
+        The summary stats as a dataframe
+    Examples
+    --------
+    >>> from samplingsimulatorpy import stat_summary
+    >>> stat_summary(pop, samples, parameter)
+    """
+
+    
