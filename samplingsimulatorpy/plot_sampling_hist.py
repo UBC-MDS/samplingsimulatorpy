2cae4d2bb2c125895e3bbb12a6da929c232eb2ec
jiangy01 <yuejiang001@outlook.com>
2020-03-06 09:38:48 -0800

    create plot_sampling_hist function
    First draft, based on Lise's `draw_samples` function. No tests written yet. Will update with tests. May need to update with any changes with the `draw_samples.py` function

---
+++ samplingsimulatorpy/plot_sampling_hist.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import altair as alt
+
+def plot_sampling_hist(samples):
+
+    """
+    Create a gird of sampling distribution histogram of the mean 
+    of different sample sizes drawn from a population
+    
+    Parameters
+    ----------
+    samples : pd.DataFrame
+        The samples as a dataframe
+        
+    Returns
+    -------
+    altair.vegalite.v3.api.FacetChart
+        A facet chart of the sampling distribution plots
+        
+    Examples
+    --------
+    >>> pop = generate_virtual_pop(1000, "Variable", normal, 0, 1)
+    >>> samples = draw_samples(pop, 100, [5, 10, 15, 20])
+    >>> plot_sampling_hist(samples, 100, [5, 10, 15, 20])
+    """
+    
+    summary = samples.groupby(['replicate', 'size', 'rep_size']).mean()
+    summary = summary.reset_index()
+    p = alt.Chart(summary).mark_bar().encode(
+        x=alt.X('values:Q', title = "mean of sampling distribution", bin = True),
+        y=alt.Y('count()')).facet(
+        facet='size', columns = 2)
+    return p
