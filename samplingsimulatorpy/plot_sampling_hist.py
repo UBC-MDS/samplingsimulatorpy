908485727b8c79c1c1e9dd78bb316cc92514a9ec
jiangy01 <yuejiang001@outlook.com>
2020-03-07 11:16:28 -0800

    update plot_sampling_hist with inputs check
    Added test to check the validity of inputs

--- samplingsimulatorpy/plot_sampling_hist.py
+++ samplingsimulatorpy/plot_sampling_hist.py
@@ -17,6 +17,13 @@ def plot_sampling_hist(samples):
     -------
     altair.vegalite.v3.api.FacetChart
         A facet chart of the sampling distribution plots
+    
+    Raises
+    -------
+    TypeError
+        if samples input is not a valid data frame    
+    ValueError
+        samples input should only contain numeric values
         
     Examples
     --------
@@ -25,10 +32,19 @@ def plot_sampling_hist(samples):
     >>> plot_sampling_hist(samples)
     """
     
+    # check the validity of input
+    if not isinstance(samples, pd.DataFrame):
+        raise TypeError("'samples' should be a dataframe")
+    
+    if not np.issubdtype(samples.to_numpy().dtype, np.number):
+        raise ValueError("'samples' should only contain numeric values")
+    
     summary = samples.groupby(['replicate', 'size', 'rep_size']).mean()
     summary = summary.reset_index()
+    x_max = summary.iloc[:,3].max()
+    x_min = summary.iloc[:,3].min()
     p = alt.Chart(summary).mark_bar().encode(
-        x=alt.X('values:Q', title = "mean of sampling distribution", bin = True),
+        x=alt.X(f"{summary.columns[3]}:Q", title = "mean of sampling distribution",bin = alt.Bin(extent=[x_min, x_max], step=1/10)),
         y=alt.Y('count()')).facet(
         facet='size', columns = 2)
     return p
