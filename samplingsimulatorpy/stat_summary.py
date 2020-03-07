e91fdc3274e8f50be6bdac032935c02e221d3f8a
tguo9 <tguo9@dons.usfca.edu>
2020-03-06 21:10:52 -0800

    Add Raises section to docstring per #38, finished docsting and add newline in the end

--- samplingsimulatorpy/stat_summary.py
+++ samplingsimulatorpy/stat_summary.py
@@ -4,6 +4,7 @@ import pandas as pd
 def stat_summary(population, samples, parameter):
     """
     This function creates a summary stats for population, samples and parameter(s) of interest
+    
     Parameters
     ----------
     population : pd.DataFrame
@@ -12,10 +13,12 @@ def stat_summary(population, samples, parameter):
         The drawed samples
     parameter : list
         The list of parameters
+    
     Returns
     -------
     pd.DataFrame
         The summary stats as a dataframe
+    
     Examples
     --------
     >>> from samplingsimulatorpy import stat_summary
