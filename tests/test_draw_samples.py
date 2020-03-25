9189f5c7c60e540b286cf037876b4c03d1f258b0
Lise Braaten <lisebraaten@gmail.com>
2020-03-25 15:35:07 -0700

    added in code comments

--- tests/test_draw_samples.py
+++ tests/test_draw_samples.py
@@ -12,20 +12,25 @@ def test_draw_samples():
     pop3 = generate_virtual_pop(10, "Height", np.random.poisson, 0)
     pop4 = generate_virtual_pop(100, 42, np.random.normal, 0, 1)
 
+    # Check that column names are as expected
     assert(all(draw_samples(pop1, 1, [1]).columns.values == (["replicate",
                                                              "Height",
                                                               "size",
                                                               "rep_size"])))
+    # Check that data frame has correct row and column length
     assert(len(draw_samples(pop2, 2, [2, 3, 4])) == 18)
     assert(len(draw_samples(pop3, 2, [2, 3]).columns) == 4)
 
+    # Check that non valid data frame will raise TypeError
     with raises(TypeError, match="Population input is not a valid data frame"):
         draw_samples(np.array([1, 2, 3]), 1, [2, 3])
 
+    # Check that if pop input is not string TypeError will be raised
     with raises(TypeError, match="Population input name "
                 "is not a valid string"):
         draw_samples(pop4, 2, [5, 10, 15, 20])
 
+    # 
     with raises(TypeError, match="Number of replications "
                 "input must be an integer value"):
         draw_samples(pop, 1.5, [2, 3])
