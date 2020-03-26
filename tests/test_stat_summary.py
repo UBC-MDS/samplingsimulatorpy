249b326dd95d2916245522b8f876ed210497054c
tguo9 <tguo9@dons.usfca.edu>
2020-03-25 20:54:21 -0700

    update tests for stat summary

--- tests/test_stat_summary.py
+++ tests/test_stat_summary.py
@@ -3,6 +3,7 @@ from samplingsimulatorpy.generate_virtual_pop import generate_virtual_pop
 from samplingsimulatorpy.draw_samples import draw_samples
 from samplingsimulatorpy.stat_summary import stat_summary
 import pytest
+import pandas as pd
 
 # Get the helper data
 pop1 = generate_virtual_pop(100, "Height", np.random.normal, 0, 1)
@@ -36,10 +37,10 @@ def test_value():
 
 def test_not_func():
     # Test if passed in a not exist function will get a AttributeError
-    with pytest.raises(AttributeError, match="module 'numpy' has "
-                       "no attribute 'mea'"):
-        stat_summary(pop1, samples1, [np.mea])
+    with pytest.raises(AttributeError):
+        stat_summary(pd.DataFrame({"A": [12, 4, 5, 44, 1]}),
+                     samples1, [np.mea])
     # Test if passed in a not exist function will get a AttributeError
-    with pytest.raises(AttributeError, match="module 'numpy' has "
-                       "no attribute 'sd'"):
-        stat_summary(pop1, samples1, [np.mean, np.sd])
+    with pytest.raises(AttributeError):
+        stat_summary(pd.DataFrame({"A": [12, 4, 5, 44, 1]}),
+                     samples1, [np.mean, np.sd])
